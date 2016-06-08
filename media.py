13407b3c894eea0ea28d90fd1fe65c1cf74e7dda
Naman Makhija <nmakhij2@illinois.edu>
2016-06-08 17:55:32 +1200

    Add files via upload
    second commit

--- media.py
+++ media.py
@@ -1,11 +1,15 @@
 import webbrowser
 
 
+#creating a class Movie
 class Movie():
-	def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = movie_poster_image
-		self.trailer_youtube_url = movie_trailer
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+	#init function
+    def __init__(self, movie_title, movie_storyline, movie_poster_image,
+                 movie_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_image
+        self.trailer_youtube_url = movie_trailer
+    #show_trailer function
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
